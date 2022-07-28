da1825867d0ab420750f6c5c670ad2c4685280e6
Billy Ponto <bpgponto@gmail.com>
2022-07-28 11:28:11 +0800

    set table transaction layout

--- resources/views/Pages/transaction.blade.php
+++ resources/views/Pages/transaction.blade.php
@@ -3,29 +3,6 @@
 @section('content')
 
     <div class="container">
-        <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
-
-        <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
-            <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
-                <div class="toast-header">
-                    <img src="..." class="rounded mr-2" alt="...">
-                    <strong class="mr-auto">Bootstrap</strong>
-                    <small>11 mins ago</small>
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="toast-body">
-                    Hello, world! This is a toast message.
-                </div>
-            </div>
-
-            @push('scripts')
-                <script>
-                    $('.toast').toast('dispose')
-                </script>
-            @endpush
-        </div>
         <div class="page-title">
             <div class="title">Transactions</div>
             <div class="subtitle">Share Transactions</div>
@@ -49,7 +26,7 @@
                             <input name="_token" type="hidden" value="{{ csrf_token() }}" />
                             <div class="form-group">
                                 <label for="dateVar">Date</label>
-                                <input type="date" class="form-control" id="dateVar" name="dateVar" required>
+                                <input type="date" class="form-control is-valid" id="dateVar" name="dateVar" required>
                             </div>
                             <div class="form-group">
                                 <label for="transactionTimeVar">Transaction Time</label>
@@ -62,7 +39,7 @@
                                     name="fromVar" data-entry="" data-account="" required>
                             </div>
                             <div class="form-group">
-                                <label for="amountVar">Net Amount (amount actually paid/received)</label>
+                                <label for="amountVar" class="max-w-max">Net Amount (amount actually paid/received)</label>
                                 <input type="number" class="form-control" id="amountVar" name="amountVar" min="0"
                                     step="any" required>
                             </div>
@@ -116,7 +93,7 @@
                         </form>
                     </div>
                     <div class="col-md-8 col-12 lg:p-40">
-                        <div class="statistics d-flex mb-5">
+                        <div class="statistics d-flex">
                             <div class="card max-w-max p-3">
                                 <div class="d-flex align-items-center">
                                     <img src="{{ asset('svgs/ic-credit-card.svg') }}" alt="">
@@ -128,8 +105,85 @@
                             </div>
                         </div>
 
+                        <div class="table-responsive __transactions-table mt-4">
+                            <table class="table table-sm table-borderless">
+                                <tr class="table-active">
+                                    <th scope="col">#</th>
+                                    <th scope="col">Transaction ID</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Transaction Time</th>
+                                    <th scope="col">Amount</th>
+                                    <th scope="col">Payee/Payor</th>
+                                </tr>
+                                <tbody>
+                                    <tr class="accordion-toggle collapsed" id="accordion1" data-toggle="collapse"
+                                        data-parent="#accordion1" href="#collapseOne">
+                                        <th scope="row">1</th>
+                                        <td class="__data-transaction-id">
+                                            <a href="#">ID-001</a>
+                                        </td>
+                                        <td>
+                                            07/22/2022
+                                        </td>
+                                        <td>
+                                            19:30:12
+                                        </td>
+                                        <td class="__data-transaction-amount">
+                                            + 2,500
+                                        </td>
+                                        <td type="button" data-toggle="collapse" data-target="#collapseExample"
+                                            aria-expanded="false" aria-controls="collapseExample"
+                                            class="d-flex align-items-center justify-content-between">
+                                            Test
+                                            <img src="{{ asset('svgs/ic_arrow-dropdown.svg') }}" alt="">
+                                        </td>
+                                    </tr>
+                                    <tr class="hide-table-padding">
+                                        <td></td>
+                                        <td colspan="5">
+                                            <div id="collapseOne" class="collapse paddingtd">
+                                                <div class="d-flex gap-3">
+                                                    <div>
+                                                        <div class="font-semibold">Details</div>
+                                                        <div style="max-width: 260px;">
+                                                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                                                            accusantium
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div class="font-semibold">No. credited</div>
+                                                        <div>
+                                                            3.000
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div class="font-semibold">No. debited name</div>
+                                                        <div>
+                                                            0.000
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div class="font-semibold">Commission</div>
+                                                        <div>
+                                                            2295.0
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div class="font-semibold">Fx</div>
+                                                        <div>
+                                                            1.0000
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
                         {{-- Empty State --}}
-                        <div class="d-flex flex-column align-items-center justify-content-center empty-state-section">
+                        <div class="d-flex flex-column align-items-center justify-content-center empty-state-section mt-5">
                             <img src="{{ asset('svgs/empty-state.svg') }}" alt="">
                             <div class="title">
                                 No Transaction Data
@@ -139,7 +193,18 @@
                             </div>
                         </div>
                     </div>
+
                 </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Modal --}}
+    <div class="modal fade" id="createCompanyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content" id="modalContent">
+
             </div>
         </div>
     </div>
